50a055dd2b8492419655151711a82cc8279d8f9d
Igor Lyubimov <lyubimovigor86@gmail.com>
2023-03-28 21:10:14 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import datetime
+from application.salary import calculate_salary
+from application.db.people import get_employees
+
+
+if __name__ == '__main__':
+    print('Текущая дата:', datetime.date.today())
+    calculate_salary()
+    get_employees()
